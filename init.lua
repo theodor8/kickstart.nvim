3385260b3910d9cd6fa4233cd5dbe2fd6cf12fca
Theodor <theodju@gmail.com>
2026-04-26 13:48:50 +0200

    tabstop

--- init.lua
+++ init.lua
@@ -166,10 +166,10 @@ vim.opt.colorcolumn = '80'
 -- See `:help 'confirm'`
 vim.o.confirm = true
 
--- vim.opt.tabstop = 4
--- vim.opt.softtabstop = 4
--- vim.opt.shiftwidth = 4
--- vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
